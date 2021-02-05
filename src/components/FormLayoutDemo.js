2ec497c4cfe96e5a9d17ca92e21ce5ac288721a0
bhaskar mannava <bhaskarmannava@bhaskars-Mac-mini.local>
2021-02-05 10:28:22 -0800

    rendering edit and non-editable fields

--- src/components/FormLayoutDemo.js
+++ src/components/FormLayoutDemo.js
@@ -38,44 +38,42 @@ export const FormLayoutDemo = () => {
                     </div>
                     <div className="p-fluid p-formgrid p-grid">
                         <div className="p-field p-col-12 p-md-6">
-                            <label htmlFor="firstname2">Title</label>
-                            {!isEdit ? <label>&nbsp;:&nbsp;{data && data.title}</label> : <InputText className={"form-input-ctrl undefined required-field form-control"}  id="firstname2" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} />}
+
+                            {!isEdit ? "" : (<div><label>Title</label><InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} /></div>)}
+                            {/*{!isEdit ? <label>&nbsp;:&nbsp;{data && data.title}0000000</label> : <InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} />}*/}
                         </div>
                         <div className="p-field p-col-12 p-md-6">
-                            <label htmlFor="lastname2">Tag text/Names</label>
-                            {!isEdit ? <label>&nbsp;:&nbsp;{data && data.tagText}</label> : <InputText id="firstname2" type="text" disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} />}
+
+                            {!isEdit ? "" :  (<div><label htmlFor="tagText">Tag text/Names</label> <InputText id="tagText" type="text" disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} /></div>)}
                         </div>
-                        <div className="p-field p-col-12 p-md-6">
+                        {/*<div className="p-field p-col-12 p-md-6">
                             <label htmlFor="videoLink">Video Link</label>
-                            {/*<InputText id="videoLink" type="text" />*/}
-                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="firstname2" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
-                            {/* <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /> */}
-                        </div>
+                            <InputText id="videoLink" type="text" />
+                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="videoLink" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
+                             <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
+                        </div>*/}
                         <div className="p-field p-col-12 p-md-3">
-                            <label htmlFor="state">Company ID's</label>
-                            {!isEdit ? <label>&nbsp;:&nbsp;{data && data.company.companyId}</label> : <Dropdown id="state" value={dropdownItem} disabled={!isEdit} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown>}
+                            {!isEdit ? "" : (<div> <label htmlFor="companyID">Company ID's</label> <Dropdown id="companyID" value={dropdownItem} disabled={!isEdit} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown></div>)}
                         </div>
                         <div className="p-field p-col-12">
-                            <label htmlFor="shortDesc">Short Description</label>
-                            {!isEdit ? <label>&nbsp;:&nbsp;{data && data.shortDesc}</label> : <InputTextarea id="shortDesc" rows="4" disabled={!isEdit} value={data && data.shortDesc} onChange={(e) => { console.log(e.target.value); setData({ ...data, shortDesc: e.target.value }) }} />}
+                            {!isEdit ? <label>{data && data.shortDesc}</label> : (<div>  <label htmlFor="shortDesc">Short Description</label> <InputTextarea id="shortDesc" rows="4" disabled={!isEdit} value={data && data.shortDesc} onChange={(e) => { console.log(e.target.value); setData({ ...data, shortDesc: e.target.value }) }} /></div>)}
                         </div>
                         <div className="p-field p-col-12">
-                            <label htmlFor="longDesc">Long Description</label>
-                            {!isEdit ? <label>&nbsp;:&nbsp;{data && data.description}</label> : <InputTextarea id="longDesc" rows="4" disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} />}
+
+                            {!isEdit ? <label>{data && data.description}</label> : (<div><label htmlFor="longDesc">Long Description</label> <InputTextarea id="longDesc" rows="4" disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} /></div>)}
                         </div>
                         <div className="p-field p-col-12 p-md-6">
-                            <label htmlFor="videoLink">Video Link</label>
-                            {/*<InputText id="videoLink" type="text" />*/}
-                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="firstname2" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
+                            {!isEdit ? <video width="320" height="240" controls>
+                                {/* <source src="{data && data.resourceLink}" type="video/mp4">
+                                <source src="{data && data.resourceLink}" type="video/ogg">*/}
+                            </video> : (<div><label>{data && data.resourceLink}</label><label htmlFor="videoLink">Video Link</label><FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /></div>)}
                             {/* <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /> */}
 
                         </div>
                         <div>
-                        <video width="320" height="240" controls>
-                           {/* <source src="{data && data.resourceLink}" type="video/mp4">
-                                <source src="{data && data.resourceLink}" type="video/ogg">*/}
-                        </video>
+
                         </div>
+                        {!isEdit ? "" : <Button label="Submit" className="p-mr-2 p-mb-2"></Button>}
                     </div>
                 </div>
             </div>
