7303090173e6285d19e3b00660a1763729f77de1
bhaskar mannava <bhaskarmannava@bhaskars-Mac-mini.local>
2021-02-05 18:17:26 -0800

    Left panel styles update

--- src/components/FormLayoutDemo.js
+++ src/components/FormLayoutDemo.js
@@ -42,26 +42,22 @@ export const FormLayoutDemo = () => {
                             {!isEdit ? "" : (<div><label>Title</label><InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} /></div>)}
                             {/*{!isEdit ? <label>&nbsp;:&nbsp;{data && data.title}0000000</label> : <InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} />}*/}
                         </div>
-                        <div className="p-field p-col-12 p-md-6">
-
-                            {!isEdit ? "" :  (<div><label htmlFor="tagText">Tag text/Names</label> <InputText id="tagText" type="text" disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} /></div>)}
-                        </div>
-                        {/*<div className="p-field p-col-12 p-md-6">
-                            <label htmlFor="videoLink">Video Link</label>
-                            <InputText id="videoLink" type="text" />
-                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="videoLink" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
-                             <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
-                        </div>*/}
                         <div className="p-field p-col-12 p-md-3">
                             {!isEdit ? "" : (<div> <label htmlFor="companyID">Company ID's</label> <Dropdown id="companyID" value={dropdownItem} disabled={!isEdit} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown></div>)}
                         </div>
                         <div className="p-field p-col-12">
                             {!isEdit ? <label>{data && data.shortDesc}</label> : (<div>  <label htmlFor="shortDesc">Short Description</label> <InputTextarea id="shortDesc" rows="4" disabled={!isEdit} value={data && data.shortDesc} onChange={(e) => { console.log(e.target.value); setData({ ...data, shortDesc: e.target.value }) }} /></div>)}
                         </div>
-                        <div className="p-field p-col-12">
 
-                            {!isEdit ? <label>{data && data.description}</label> : (<div><label htmlFor="longDesc">Long Description</label> <InputTextarea id="longDesc" rows="4" disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} /></div>)}
-                        </div>
+                        {/*<div className="p-field p-col-12 p-md-6">
+                            <label htmlFor="videoLink">Video Link</label>
+                            <InputText id="videoLink" type="text" />
+                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="videoLink" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
+                             <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
+                        </div>*/}
+
+
+
                         <div className="p-field p-col-12 p-md-6">
                             {!isEdit ? <video width="320" height="240" controls>
                                 {/* <source src="{data && data.resourceLink}" type="video/mp4">
@@ -70,8 +66,13 @@ export const FormLayoutDemo = () => {
                             {/* <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /> */}
 
                         </div>
-                        <div>
+                        <div className="p-field p-col-12 p-md-6">
 
+                            {!isEdit ? "" :  (<div><label htmlFor="tagText">Tag text/Names</label> <InputTextarea rows="5.5"  id="tagText" type="text" disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} /></div>)}
+                        </div>
+                        <div className="p-field p-col-12">
+
+                            {!isEdit ? <label>{data && data.description}</label> : (<div><label htmlFor="longDesc">Long Description</label> <InputTextarea id="longDesc" rows="8" disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} /></div>)}
                         </div>
                         {!isEdit ? "" : <Button label="Submit" className="p-mr-2 p-mb-2"></Button>}
                     </div>
