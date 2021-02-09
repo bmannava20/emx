5c91460d00e1f4ed052f932c9babba79d0ea9fcf
bhaskar mannava <bhaskarmannava@bhaskars-Mac-mini.local>
2021-02-08 20:10:12 -0800

    adding buttons on edit view

--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -19,11 +19,11 @@ const AppTopbar = (props) => {
                     <AppBreadcrumb routers={props.routers} />
                 </div>
 
-                <img id="logo-mobile" className="mobile-logo" src="assets/layout/images/logo-dark.svg" alt="diamond-layout" />
+                <img id="logo-mobile" className="mobile-logo" src="assets/layout/images/logo-dark.svg" />
             </div>
 
-            <div className="topbar-right">
-                {/*<ul className="topbar-menu">
+            {/*<div className="topbar-right">
+                <ul className="topbar-menu">
                     <li className="search-item">
                         <button type="button" className="p-link" onClick={props.onSearchClick}>
                             <i className="pi pi-search"></i>
@@ -127,8 +127,8 @@ const AppTopbar = (props) => {
                             <i className="pi pi-align-right"></i>
                         </button>
                     </li>
-                </ul>*/}
-            </div>
+                </ul>
+            </div>*/}
         </div>
     );
 }
--- src/components/FormLayoutDemo.js
+++ src/components/FormLayoutDemo.js
@@ -34,47 +34,53 @@ export const FormLayoutDemo = () => {
                 <div className="card">
                     <div className="p-fluid p-formgrid p-grid">
                         <h5 className="p-field p-col-12 p-md-11">{data && data.title} ({data && data.company.companyId})</h5>
-                        <div className="p-field p-col-12 p-md-1">{!isEdit ? <Button label="Edit" onClick={() => { setIsEdit(!isEdit) }}></Button> : <Button onClick={() => { setIsEdit(!isEdit) }} label="Save"></Button>}</div>
+                        <div className="p-field p-col-12 p-md-1">{!isEdit ? <Button label="Edit" onClick={() => { setIsEdit(!isEdit) }}></Button> : <Button onClick={() => { setIsEdit(!isEdit) }} label="Cancel"></Button>}</div>
                     </div>
                     <div className="p-fluid p-formgrid p-grid">
                         <div className="p-field p-col-12 p-md-6">
 
-                            {!isEdit ? "" : (<div><label>Title</label><InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} /></div>)}
+                            {!isEdit ? "" : (<div><label>Title</label><InputText className={"form-input-ctrl required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} /></div>)}
                             {/*{!isEdit ? <label>&nbsp;:&nbsp;{data && data.title}0000000</label> : <InputText className={"form-input-ctrl undefined required-field form-control"}  id="tittle" type="text" disabled={!isEdit} value={data && data.title} onChange={(e) => { console.log(e.target.value); setData({ ...data, title: e.target.value }) }} />}*/}
                         </div>
                         <div className="p-field p-col-12 p-md-3">
-                            {!isEdit ? "" : (<div> <label htmlFor="companyID">Company ID's</label> <Dropdown id="companyID" value={dropdownItem} disabled={!isEdit} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown></div>)}
+                            {!isEdit ? "" : (<div> <label htmlFor="companyID">Company ID's</label> <Dropdown id="companyID" className={"form-input-ctrl required-field form-control drpdownwidth"} value={dropdownItem} disabled={!isEdit} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown></div>)}
                         </div>
+                    </div>
+                    <div className="p-fluid p-formgrid p-grid">
                         <div className="p-field p-col-12">
-                            {!isEdit ? <label>{data && data.shortDesc}</label> : (<div>  <label htmlFor="shortDesc">Short Description</label> <InputTextarea id="shortDesc" rows="4" disabled={!isEdit} value={data && data.shortDesc} onChange={(e) => { console.log(e.target.value); setData({ ...data, shortDesc: e.target.value }) }} /></div>)}
+                            {!isEdit ? <label>{data && data.shortDesc}</label> : (<div>  <label htmlFor="shortDesc">Short Description</label> <InputTextarea id="shortDesc" rows="4" className={"form-control required-field"} disabled={!isEdit} value={data && data.shortDesc} onChange={(e) => { console.log(e.target.value); setData({ ...data, shortDesc: e.target.value }) }} /></div>)}
                         </div>
-
-                        {/*<div className="p-field p-col-12 p-md-6">
-                            <label htmlFor="videoLink">Video Link</label>
-                            <InputText id="videoLink" type="text" />
-                            {!isEdit ? <label>{data && data.resourceLink}</label> : <InputText id="videoLink" type="text" disabled={!isEdit} value={data && data.resourceLink} onChange={(e) => { console.log(e.target.value); setData({ ...data, resourceLink: e.target.value }) }} />}
-                             <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
-                        </div>*/}
-
-
-
+                    </div>
+                    <div className="p-fluid p-formgrid p-grid">
                         <div className="p-field p-col-12 p-md-6">
                             {!isEdit ? <video width="320" height="240" controls>
                                 {/* <source src="{data && data.resourceLink}" type="video/mp4">
-                                <source src="{data && data.resourceLink}" type="video/ogg">*/}
+                                    <source src="{data && data.resourceLink}" type="video/ogg">*/}
                             </video> : (<div><label>{data && data.resourceLink}</label><label htmlFor="videoLink">Video Link</label><FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /></div>)}
                             {/* <FileUpload name="demo[]" disabled={!isEdit} url="./upload.php" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} /> */}
 
                         </div>
                         <div className="p-field p-col-12 p-md-6">
 
-                            {!isEdit ? "" :  (<div><label htmlFor="tagText">Tag text/Names</label> <InputTextarea rows="5.5"  id="tagText" type="text" disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} /></div>)}
+                            {!isEdit ? "" :  (<div><label htmlFor="tagText">Tag text/Names</label> <InputTextarea rows="5.5"  id="tagText" type="text" className={"form-control"} disabled={!isEdit} value={data && data.tagText} onChange={(e) => { console.log(e.target.value); setData({ ...data, tagText: e.target.value }) }} /></div>)}
                         </div>
                         <div className="p-field p-col-12">
 
-                            {!isEdit ? <label>{data && data.description}</label> : (<div><label htmlFor="longDesc">Long Description</label> <InputTextarea id="longDesc" rows="8" disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} /></div>)}
+                            {!isEdit ? <label>{data && data.description}</label> : (<div><label htmlFor="longDesc">Long Description</label> <InputTextarea id="longDesc" rows="8" className={"form-control"} disabled={!isEdit} value={data && data.description} onChange={(e) => { console.log(e.target.value); setData({ ...data, description: e.target.value }) }} /></div>)}
                         </div>
-                        {!isEdit ? "" : <Button label="Submit" className="p-mr-2 p-mb-2"></Button>}
+                    </div>
+                    <div className="p-fluid p-formgrid p-grid">
+                        <div className="p-field p-col-9 p-md-9"></div>
+                        <div className="p-field p-col-2 p-md-1">
+                            {!isEdit ? "" : <Button label="Reset"></Button>}
+                        </div>
+
+                        <div className="p-field p-col-2 p-md-1">
+                            {!isEdit ? "" : <Button label="Cancel"></Button>}
+                        </div>
+                        <div className="p-field p-col-2 p-md-1">
+                            {!isEdit ? "" : <Button label="Submit"></Button>}
+                        </div>
                     </div>
                 </div>
             </div>
